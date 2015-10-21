274e6dafb5dfdea139820993b3bf0549df0b744c
Benson Mburu <nguruben@gmail.com>
2015-10-21 11:06:34 +0300

    added connection to mysql

---
+++ Config.php
@@ -0,0 +1,9 @@
+<?php
+ 
+ //Variables needed for database connection
+ define("DB_HOST", "localhost");
+ define("DB_USER", "root");
+ define("DB_PASSWORD", "master12!");
+ define("DB_DATABASE", "loginAPI");
+ 
+?>
